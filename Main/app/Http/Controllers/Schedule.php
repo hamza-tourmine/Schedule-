58835ee611e27194d0aa2ac21a051b0938fed9d5
hamza tourmine <hamzatourmine9@gmail.com>
2024-01-27 23:58:24 +0100

    confli when i merge

--- Main/app/Http/Controllers/Schedule.php
+++ Main/app/Http/Controllers/Schedule.php
@@ -30,7 +30,7 @@ class Schedule extends Controller
 
         $establishment_id = session()->get('establishment_id');
         $classType = class_room_type::all()->where('establishment_id',$establishment_id);
-        return  view('adminDashboerd.mane.mane',['formateurs'=>$formateurs,'groups'=>$groups,'modules'=>$modules,'salles'=>$salles,'classType'=>$classType]);
+        return  view('adminDashboerd.main.main',['formateurs'=>$formateurs,'groups'=>$groups,'modules'=>$modules,'salles'=>$salles,'classType'=>$classType]);
     }
 
     public function insertSession(Request $request){
