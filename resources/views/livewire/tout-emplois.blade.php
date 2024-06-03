a30898a12b13d6aea01da9c0e0beb844038126c6
Ahmed Errebache <ahmed.errebache1@gmail.com>
2024-06-03 22:42:22 +0100

    remove pop up from back up

--- resources/views/livewire/tout-emplois.blade.php
+++ resources/views/livewire/tout-emplois.blade.php
@@ -296,7 +296,6 @@
                         <th colspan="2">Matin </th>
                         <th colspan="2">A.midi </th>
                         @endfor
-
                     </tr>
                     <tr class="se-row">
                              @for ($i =0 ; $i<12 ; $i++ )
@@ -309,149 +308,6 @@
                     @php
                      $dayWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
                 @endphp
-                {{-- Model start --}}
-                      <div style="z-index: 999999999" wire:ignore.self  class="modal fade col-9" id="exampleModal" tabindex="-1"
-                      aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      {{-- live wire  for diplay  new model update model  --}}
-
-                      <div class="modal-dialog  modal-lg  ">
-                        <div class="modal-content  col-9">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" >Create session</h1>
-                                    @if ($errors->any())
-                                    @foreach ( $errors->all() as $error)
-                                    <div id="liveAlertPlaceholder" class="alert alert-danger">
-                                        {{$error}}
-                                    </div>
-                              @endforeach
-                              @endif
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <form wire:submit.prevent="UpdateSession">
-                                <div class="modal-body">
-                                    {{-- branches  --}}
-                                @if($selectedType!=='Group')
-                                @if (!$checkValues[0]->branch)
-                                    <select wire:model='brancheId'  class="form-select "  aria-label="Default select example">
-                                        <option > Filiére</option>
-                                        @if ($baranches)
-                                        @foreach ($baranches as $baranche)
-                                        <option value="{{ $baranche->id }}">{{ $baranche->name }}</option>
-                                        @endforeach
-                                        @endif
-                                    </select >
-                                    @endif
-
-                                    @if (!$checkValues[0]->year)
-                                    <select wire:model='selectedYear'  class="form-select "  aria-label="Default select example">
-                                        <option > année </option>
-                                        @if ($yearFilter)
-                                        @foreach ($yearFilter as $item)
-                                        <option value="{{ $item }}">{{ $item}}</option>
-                                        @endforeach
-                                        @endif
-                                    </select >
-                                    @endif
-                                @endif
-
-                                    <div style="display: flex">
-
-
-                                        {{-- Formateur --}}
-                                        @if($selectedType==='Group')
-                                        <select wire:model='formateur' class="form-select"
-                                            aria-label="Default select example">
-
-                                                <option selected>Formateurs</option>
-                                                    @foreach ($Group_has_formateurs as $formateur)
-                                                        <option value="{{ $formateur->id }}">
-                                                            {{ $formateur->user_name }}</option>
-                                                    @endforeach
-                                        </select>
-                                        @else
-                                        <select wire:model="group" class="form-select" aria-label="Default select example">
-                                            <option value="" selected>Groupes</option>
-                                            @if(!$groupes->isEmpty())
-                                                @foreach ($groupes as $grp)
-                                                    <option value="{{ $grp->id }}">{{ $grp->group_name }}</option>
-                                                @endforeach
-                                            @else
-                                                <option>Pas de groupe trouvé <i style="color:black" class="mdi mdi-alert-rhombus"></i></option>
-                                            @endif
-                                        </select>
-
-
-                                    @endif
-
-
-                                      {{-- module  content --}}
-                                      @if (!$checkValues[0]->module)
-                                      <select wire:model="module" class="form-select "
-                                          aria-label="Default select example">
-                                          <option selected>Modules</option>
-                                          @if ($modules)
-                                              @foreach ($modules as $module)
-                                                  <option value="{{ $module->id }}">
-                                                    {{ preg_replace('/^\d+/' , '' ,$module->id )}}</option>
-                                              @endforeach
-                                          @endif
-                                      </select>
-                                      @endif
-
-                                        {{-- salle --}}
-                                        @if (!$checkValues[0]->salle)
-                                        <select wire:model="salle" class="form-select"
-                                            aria-label="Default select example">
-                                            <option selected>les salles</option>
-                                            @if ($salles)
-                                                @foreach ($salles as $salle)
-                                                    <option value="{{ $salle->id }}">
-                                                        {{ $salle->class_name }}</option>
-                                                @endforeach
-                                            @endif
-                                        </select>
-                                        @endif
-                                    </div>
-                                    {{-- tyope session --}}
-                                    <div style="display: flex;justify-content: space-between">
-                                        @if (!$checkValues[0]->typeSalle)
-                                        <select wire:model="salleclassTyp" class="form-select"
-                                            aria-label="Default select example">
-                                            <option selected> Type de salle</option>
-                                            @if ($classType)
-                                                @foreach ($classType as $classTyp)
-                                                    <option value="{{ $classTyp->class_room_types }}">
-                                                        {{ $classTyp->class_room_types }}</option>
-                                                @endforeach
-                                            @endif
-                                        </select>
-                                        @endif
-                                        {{-- id case --}}
-                                        <input type="hidden"   value="{{$receivedVariable}}" >
-                                    </div>
-                                    {{-- day part && type sission --}}
-                                    <div style="display: flex">
-                                        @if (!$checkValues[0]->typeSession)
-                                        <select wire:model="TypeSesion" class="form-select"
-                                            aria-label="Default select example">
-                                            <option selected>Type  de Séance</option>
-                                            <option value="PRESENTIEL">Presentielle</option>
-                                            <option value="teams">Teams</option>
-                                             <option value="EFM">EFM</option>
-                                        </select>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button data-bs-dismiss="modal" wire:click="DeleteSession" aria-label="Close" type="button"  class="btn btn-danger">supprimer</button>
-                                    <button data-bs-dismiss="modal" wire:click="UpdateSession" aria-label="Close" type="submit"  class="btn btn-success">Updare</button>
-                                </div>
-                        </form>
-                </div>
-                </div>
-                  </div>
-
    {{-- FOR GROUPES  --}}
    @if($selectedType === 'Group')
    @foreach ($groups as $group)
@@ -474,7 +330,7 @@
             @endif
         @endforeach
 
-        <td style="background-color: {{ $sessionFound ? 'rgba(12, 72, 166, 0.3);' : '' }}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="Cases {{ $day }}" wire:click="getidCase('{{ $day.$sessionType.$group->id }}')" id="{{ $day.$sessionType.$group->id }}">
+        <td style="background-color: {{ $sessionFound ? 'rgba(12, 72, 166, 0.3);' : '' }}" id="{{ $day.$sessionType.$group->id }}">
             @if ($sessionFound && $currentSission)
                 <span>{{ $currentSission->sission_type }}</span>
                 <span>{{ $currentSission->user_name }}</span>
